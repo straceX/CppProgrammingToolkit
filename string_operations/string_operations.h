6d546509b48ea833514430f1311f9672424684c8
int 0x80 <netsecdeveloper@gmail.com>
2016-10-31 20:01:52 +0300

    string is a rotated form of another string control function added
    std::cout << "Hello World C++11 Repo" <<std::endl;

---
+++ string_operations/string_operations.h
@@ -0,0 +1,5 @@
+#include <string>
+
+/* Lets write test code that test whether a string is a rotated form of another string*/
+/*result must be oneliner :)*/
+bool is_rotate(const std::string &s1, const std::string &s2);
