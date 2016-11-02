8ee6b7795cc08caa3eb91c45d4402f84ee8662ad
int 0x80 <netsecdeveloper@gmail.com>
2016-11-02 21:05:43 +0300

    character & string counter functions included
    character & string counter two functions included

--- string_operations/string_operations.cpp
+++ string_operations/string_operations.cpp
@@ -3,3 +3,17 @@ bool is_rotate(const std::string &s1, const std::string &s2)
 {
 	return s1.size() == s2.size() && std::equal(s1.begin(), s1.end(), s2.rbegin());		
 }
+
+size_t count_character(const std::string &sparam,char ch)
+{
+	return std::count(sparam.begin(), sparam.end(), ch);
+}
+
+size_t count_words(const std::string &sparam, const std::string &sword)
+{
+	size_t iter = 0;
+	size_t ival = 0;
+	while ((iter = sparam.find(sword, ++iter)) != std::string::npos)
+		++ival;
+	return ival;
+}
--- string_operations/string_operations.h
+++ string_operations/string_operations.h
@@ -3,3 +3,9 @@
 /* Lets write test code that test whether a string is a rotated form of another string*/
 /*result must be oneliner :)*/
 bool is_rotate(const std::string &s1, const std::string &s2);
+
+/*Find how many character include*/
+size_t count_character(const std::string &sparam,char ch);
+
+/*Find how many words include*/
+size_t count_words(const std::string &sparam, const std::string &sword);
