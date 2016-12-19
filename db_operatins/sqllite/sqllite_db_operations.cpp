bd6dd450ff5d515d461807dd6034c3c91f2b3cc2
int 0x80 <netsecdeveloper@gmail.com>
2016-12-19 12:16:02 +0300

    refactoring and add new functions
    bool exec_querry(std::string querry);
    bool create_table(std::string querry);
    bool update_table(std::string querry);
    bool delete_table(std::string querry);

--- db_operatins/sqllite/sqllite_db_operations.cpp
+++ db_operatins/sqllite/sqllite_db_operations.cpp
@@ -1,50 +1,103 @@
 #include "sqllite_db_operations.h"
 
-class sqllitedb {
-private:
-    sqlite3 *db;
-    const std::string dbname;
-public:
-    sqllitedb(const std::string &dbname) : dbname(dbname)
-    {
-        try {
 
-            if (sqlite3_open(dbname.c_str(), &db) != SQLITE_OK)
-            {
-                throw sqlite3_errmsg(db);
-            }
 
-        }
-        catch (const char* msg) {
-            std::cerr << msg << std::endl;
-            sqlite3_close(db);
+sqllitedb::sqllitedb(const std::string &dbname) : dbname(dbname)
+{
+    try {
+
+        if (sqlite3_open(dbname.c_str(), &db) != SQLITE_OK)
+        {
+            throw sqlite3_errmsg(db);
         }
 
     }
-
-    std::string db_version()
-    {
-        return std::string(sqlite3_libversion());
+    catch (const char* msg) {
+        std::cerr << msg << std::endl;
+        sqlite3_close(db);
     }
 
-    bool db_exec(std::string querry)
-    {
-        char *errMsg;
-        try {
+}
 
-            if (sqlite3_exec(db, querry.c_str(), NULL, NULL, &errMsg) != SQLITE_OK)
-            {
-                throw errMsg;
-            }
-        }
-        catch (const char* msg) {
-            std::cerr << msg << std::endl;
-            sqlite3_free(errMsg);
-            sqlite3_close(db);
-            return false;
-        }
+std::string sqllitedb::db_version()
+{
+    return std::string(sqlite3_libversion());
+}
 
-        return true;
+bool sqllitedb::exec_querry(std::string querry)
+{
+    try {
+
+         if (sqlite3_exec(db, querry.c_str(), NULL, NULL, &errMsg) != SQLITE_OK)
+         {
+            throw errMsg;
+         }
+         std::cout << "Querry execed" << std::endl;
+
+    } catch (const char* msg) {
+         std::cerr << msg << std::endl;
+         sqlite3_free(errMsg);
+         sqlite3_close(db);
+         return false;
     }
 
-};
+    return true;
+}
+bool sqllitedb::create_table(std::string querry)
+{
+    try {
+
+        if (sqlite3_exec(db, querry.c_str(), NULL, NULL, &errMsg) != SQLITE_OK)
+        {
+           throw errMsg;
+        }
+        std::cout << "Table created" << std::endl;
+
+    } catch (const char* msg) {
+        std::cerr << msg << std::endl;
+        sqlite3_free(errMsg);
+        sqlite3_close(db);
+        return false;
+    }
+    return true;
+}
+
+bool sqllitedb::update_table(std::string querry)
+{
+    try {
+
+        if (sqlite3_exec(db, querry.c_str(), NULL, NULL, &errMsg) != SQLITE_OK)
+        {
+           throw errMsg;
+        }
+        std::cout << "Table updated" << std::endl;
+
+    } catch (const char* msg) {
+        std::cerr << msg << std::endl;
+        sqlite3_free(errMsg);
+        sqlite3_close(db);
+        return false;
+    }
+    return true;
+}
+
+bool sqllitedb::delete_table(std::string querry)
+{
+    try {
+
+        if (sqlite3_exec(db, querry.c_str(), NULL, NULL, &errMsg) != SQLITE_OK)
+        {
+           throw errMsg;
+        }
+        std::cout << "Table deleted" << std::endl;
+
+    } catch (const char* msg) {
+        std::cerr << msg << std::endl;
+        sqlite3_free(errMsg);
+        sqlite3_close(db);
+        return false;
+    }
+    return true;
+}
+
+
--- db_operatins/sqllite/sqllite_db_operations.h
+++ db_operatins/sqllite/sqllite_db_operations.h
@@ -1,2 +1,26 @@
+#ifndef SQLLITE_DB_OPERATIONS_H
+#define SQLLITE_DB_OPERATIONS_H
+
 #include "sqlite3.h"
 #include <iostream>
+
+class sqllitedb {
+private:
+    sqlite3 *db;
+    const std::string dbname;
+    char *errMsg;
+public:
+    sqllitedb(const std::string &dbname);
+
+    std::string db_version();
+
+    bool exec_querry(std::string querry);
+
+    bool create_table(std::string querry);
+    bool update_table(std::string querry);
+    bool delete_table(std::string querry);
+
+};
+
+
+#endif // SQLLITE_DB_OPERATIONS_H
