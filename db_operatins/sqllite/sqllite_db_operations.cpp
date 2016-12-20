c8942a27607217f2dbe32ffcf6b2511f8b4e60cc
int 0x80 <netsecdeveloper@gmail.com>
2016-12-20 13:55:17 +0300

    New function added
    table_row_count,db_table_list

--- db_operatins/sqllite/sqllite_db_operations.cpp
+++ db_operatins/sqllite/sqllite_db_operations.cpp
@@ -1,7 +1,5 @@
 #include "sqllite_db_operations.h"
 
-
-
 sqllitedb::sqllitedb(const std::string &dbname) : dbname(dbname)
 {
     try {
@@ -22,6 +20,50 @@ sqllitedb::sqllitedb(const std::string &dbname) : dbname(dbname)
 std::string sqllitedb::db_version()
 {
     return std::string(sqlite3_libversion());
+}
+
+bool sqllitedb::table_row_count(std::string table_name,int &row_count)
+{
+    std::string querry = "select count(*) from " + table_name;
+
+    try {
+
+         if (sqlite3_exec(db, querry.c_str(), callback, &row_count, &errMsg) != SQLITE_OK)
+         {
+            throw errMsg;
+         }
+         std::cout << "Querry execed" << std::endl;
+
+    } catch (const char* msg) {
+         std::cerr << msg << std::endl;
+         sqlite3_free(errMsg);
+         sqlite3_close(db);
+         return false;
+    }
+
+    return true;
+
+}
+
+bool sqllitedb::db_table_list()
+{
+    std::string querry = "SELECT name FROM my_db.sqlite_master WHERE type='table'";
+
+    try {
+
+         if (sqlite3_exec(db, querry.c_str(), callback, NULL, &errMsg) != SQLITE_OK)
+         {
+            throw errMsg;
+         }
+
+    } catch (const char* msg) {
+         std::cerr << msg << std::endl;
+         sqlite3_free(errMsg);
+         sqlite3_close(db);
+         return false;
+    }
+
+    return true;
 }
 
 bool sqllitedb::exec_querry(std::string querry)
@@ -101,3 +143,23 @@ bool sqllitedb::delete_table(std::string querry)
 }
 
 
+sqllitedb::sqllitedb(const sqllitedb &sqllitedbObj)
+{
+    return sqllitedb(sqllitedbObj.dbname);
+}
+
+sqllitedb &sqllitedb::operator=(const sqllitedb &sqllitedbObj)
+{
+    this->db = sqllitedbObj.db;
+    this->column_count = sqllitedbObj.column_count;
+    this->table_list = sqllitedbObj.table_list;
+    this->dbname = sqllitedbObj.dbname;
+    this->table = sqllitedbObj.table;
+    this->row_count = sqllitedbObj.row_count;
+    this->column_count = sqllitedbObj.column_count;
+}
+
+sqllitedb::~sqllitedb()
+{
+    /*will write*/
+}
--- db_operatins/sqllite/sqllite_db_operations.h
+++ db_operatins/sqllite/sqllite_db_operations.h
@@ -2,23 +2,64 @@
 #define SQLLITE_DB_OPERATIONS_H
 
 #include "sqlite3.h"
-#include <iostream>
 
+#include <iostream>
+#include <vector>
+#include <map>
 class sqllitedb {
 private:
     sqlite3 *db;
     const std::string dbname;
     char *errMsg;
+    std::vector< std::map<std::string,std::string> > table;
+
+    size_t row_count = 0;
+    size_t column_count = 0;
+
+
+
+    int callback(void *arg, int argc, char **argv, char **colName)
+    {
+        int *c = arg;
+        *c = atoi(argv[0]);
+        return 0;
+    }
+
+    int callback2(void *arg, int argc, char **argv, char **colName)
+    {
+        std::map<std::string,std::string> row;
+        for(int i=0; i<argc; i++)
+        {
+            row.insert(std::pair<colName[i],argv[i] ? argv[i] : "NULL">);
+            if(!(i%row_count))
+            {
+                table.push_back(row);
+                row.clear();
+            }
+        }
+        return 0;
+    }
+
+
 public:
+
     sqllitedb(const std::string &dbname);
+    sqllitedb(const sqllitedb &sqllitedbObj);
+    sqllitedb &operator=(const sqllitedb &sqllitedbObj);
+    ~sqllitedb();
 
     std::string db_version();
+    bool sqllitedb::table_row_count(std::string table_name,int &row_count);
+    bool sqllitedb::db_table_list();
 
     bool exec_querry(std::string querry);
 
     bool create_table(std::string querry);
     bool update_table(std::string querry);
     bool delete_table(std::string querry);
+
+protected:
+    std::vector<std::string> table_list;
 
 };
 
